84fccfa69dad70398a810b11202a0b555935472a
Vinicius-O-Neves <vinicius.neeves@outlook.com>
2023-03-22 12:00:05 -0300

    added build files

--- buildSrc/src/main/kotlin/Compose.kt
+++ buildSrc/src/main/kotlin/Compose.kt
@@ -4,6 +4,7 @@ object Compose {
 
     const val composeActivity = "androidx.activity:activity-compose:1.6.1"
     const val composeUI = "androidx.compose.ui:ui:$composeUIVersion"
+    const val composeUITolling = "androidx.compose.ui:ui-tooling:1.2.0"
     const val composeUITollingPreview = "androidx.compose.ui:ui-tooling-preview:$composeUIVersion"
     const val composeMaterial = "androidx.compose.material:material:1.3.1"
     const val composeMaterial3 = "androidx.compose.material3:material3:1.0.0"
