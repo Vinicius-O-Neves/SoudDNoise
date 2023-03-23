5492dd7b33f8dc9ba49cfe59e532069311dc5125
Vinicius-O-Neves <vinicius.neeves@outlook.com>
2023-03-23 12:42:07 -0300

    Created annotation class

---
+++ shared/presentation/src/main/java/com/example/presentation/sounddnoise/theme/SoundDNoiseThemes.kt
@@ -0,0 +1,8 @@
+package com.example.presentation.sounddnoise.theme
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(group = "SoundDNoise", uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(group = "SoundDNoise", uiMode = Configuration.UI_MODE_NIGHT_YES)
+annotation class SoundDNoiseThemes()
