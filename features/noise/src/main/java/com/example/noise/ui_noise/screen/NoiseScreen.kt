95dbbacfd8ce61b1eddf10a12a284c5ed32c9bd5
Vinicius-O-Neves <vinicius.neeves@outlook.com>
2023-04-02 21:28:50 -0300

    Added lottie animation

--- features/noise/src/main/java/com/example/noise/ui_noise/screen/NoiseScreen.kt
+++ features/noise/src/main/java/com/example/noise/ui_noise/screen/NoiseScreen.kt
@@ -2,63 +2,56 @@ package com.example.noise.ui_noise.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.noise.ui_noise.NoiseState
 import com.example.noise.ui_noise.components.AudioSpectrum
 import com.example.noise.ui_noise.model.FrequencyState
 import com.example.presentation.app.AppTheme
+import com.example.presentation.components.lottie.AppLottieAnimation
 import com.example.presentation.components.spacing.AppSpacing
 import com.example.presentation.sounddnoise.theme.SoundDNoiseTheme
 import com.example.presentation.sounddnoise.theme.SoundDNoiseThemes
-import java.util.*
+import com.example.sounddnoise.R
 
 @Composable
-fun NoiseScreen(frequencyState: FrequencyState, audioDecibel: Double) {
+fun NoiseScreen(frequencyState: FrequencyState, audioDecibel: Double, noiseState: NoiseState) {
     NoiseScreenContent(
-        frequenciesState = frequencyState,
-        audioDecibel = audioDecibel
+        frequenciesState = { frequencyState }, audioDecibel = audioDecibel, noiseState = noiseState
     )
 }
 
 @Composable
-private fun NoiseScreenContent(frequenciesState: FrequencyState, audioDecibel: Double) {
-    val formattedAudioDecibel =
-        String.format(locale = Locale.getDefault(), format = "%.2f", audioDecibel)
-
+private fun NoiseScreenContent(
+    frequenciesState: () -> FrequencyState, audioDecibel: Double, noiseState: NoiseState
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .background(AppTheme.colors.background)
             .fillMaxSize()
     ) {
+        val lottieAnimation = when (noiseState) {
+            NoiseState.LOW -> R.raw.ic_calm_backdrop
+            NoiseState.MEDIUM -> R.raw.ic_warning
+            NoiseState.HIGH -> R.raw.ic_head_explosion
+        }
+
+        AppLottieAnimation(
+            lottieFile = lottieAnimation,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(500.dp)
+                .padding(AppSpacing.base)
+        )
+
         Spacer(modifier = Modifier.weight(1f))
 
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .padding(bottom = AppSpacing.base)
-                .fillMaxWidth()
-        ) {
-            Text(
-                style = MaterialTheme.typography.h4.copy(
-                    color = AppTheme.colors.onBackground,
-                    fontWeight = FontWeight.Bold
-                ),
-                text = formattedAudioDecibel.plus(" Db"),
-            )
-
-            Spacer(
-                modifier = Modifier
-                    .height(AppSpacing.xlarge)
-            )
-
+        Column {
             AudioSpectrum(
-                frequenciesArray = frequenciesState.frequencies
+                frequenciesState = frequenciesState, audioDecibel = audioDecibel
             )
         }
     }
@@ -70,9 +63,8 @@ private fun NoiseScreen_Previews() {
     SoundDNoiseTheme {
         NoiseScreen(
             frequencyState = FrequencyState(
-                frequencies = doubleArrayOf(1.2, 190.0, 12.2, 0.1, 25.0)
-            ),
-            audioDecibel = 40.023214
+                frequencies = doubleArrayOf(1.2, 190.0, 12.2, 0.1, 25.0),
+            ), audioDecibel = 40.023214, noiseState = NoiseState.HIGH
         )
     }
 }
