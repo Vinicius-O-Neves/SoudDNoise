8c05079ff5ce7e14fc80c1fc624bbec21d56e420
Lucas <lbonifaciofreitas@gmail.com>
2023-04-07 12:21:59 -0300

    Removed the unused imports, Fixed the layout to work with the new text notification, Added warning text

--- features/noise/src/main/java/com/example/noise/ui_noise/screen/NoiseScreen.kt
+++ features/noise/src/main/java/com/example/noise/ui_noise/screen/NoiseScreen.kt
@@ -2,9 +2,13 @@ package com.example.noise.ui_noise.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.noise.ui_noise.NoiseState
 import com.example.noise.ui_noise.components.AudioSpectrum
@@ -33,21 +37,38 @@ private fun NoiseScreenContent(
             .background(AppTheme.colors.background)
             .fillMaxSize()
     ) {
-        val lottieAnimation = when (noiseState) {
-            NoiseState.LOW -> R.raw.ic_calm_backdrop
-            NoiseState.MEDIUM -> R.raw.ic_warning
-            NoiseState.HIGH -> R.raw.ic_head_explosion
-        }
+
 
         Column(
-            verticalArrangement = Arrangement.Top
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier
+                .padding(AppSpacing.base)
+                .fillMaxWidth()
         ) {
+
+            val warning = when (noiseState) {
+                NoiseState.LOW -> R.string.warning_low
+                NoiseState.MEDIUM -> R.string.warning_medium
+                NoiseState.HIGH -> R.string.warning_high
+            }
+
+            Text(
+                style = MaterialTheme.typography.h6.copy(
+                    color = AppTheme.colors.onBackground
+                ), text = stringResource(id = warning), textAlign = TextAlign.Center
+            )
+
+            val lottieAnimation = when (noiseState) {
+                NoiseState.LOW -> R.raw.ic_calm_backdrop
+                NoiseState.MEDIUM -> R.raw.ic_warning
+                NoiseState.HIGH -> R.raw.ic_head_explosion
+            }
+
             AppLottieAnimation(
                 lottieFile = lottieAnimation,
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .padding(top = AppSpacing.regular)
                     .heightIn(max = 300.dp)
-                    .padding(AppSpacing.base)
             )
         }
 
