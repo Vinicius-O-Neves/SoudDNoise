a19f20e02bafe437e5e22b20b022076e737e4445
Vinicius-O-Neves <vinicius.neeves@outlook.com>
2023-05-28 21:16:59 -0300

    Added firestore integration and added data to heat map

--- app/src/main/java/br/puc/soundmap/MainActivity.kt
+++ app/src/main/java/br/puc/soundmap/MainActivity.kt
@@ -1,28 +1,28 @@
 package br.puc.soundmap
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import br.puc.soundmap.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.maps.android.heatmaps.HeatmapTileProvider
-import com.google.android.gms.maps.model.TileOverlayOptions
 import com.google.android.gms.maps.model.LatLng
-
-
-
+import com.google.android.gms.maps.model.TileOverlayOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.maps.android.heatmaps.HeatmapTileProvider
+import com.google.maps.android.heatmaps.WeightedLatLng
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private val database = Firebase.firestore
+
+    private var mGoogleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,19 +30,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        val timer = Timer()
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val task = object : TimerTask() {
+            override fun run() {
+                getDataFromFirebase()
+            }
         }
 
+        timer.schedule(task, 0, 2000)
+
         // Obtenha uma referência ao fragmento do mapa
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
 
         // Solicite o carregamento assíncrono do mapa
         mapFragment.getMapAsync { googleMap ->
@@ -50,43 +50,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+
+        val markerPosition = LatLng(37.4219983, -122.084)
+        mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, 12f))
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun getDataFromFirebase() {
+        val collectionPath = "/data/average_data/locations"
+
+        val collectionRef = database.collection(collectionPath)
+
+        val heatMapData = mutableListOf<WeightedLatLng>()
+
+        collectionRef.get().addOnSuccessListener { querySnapshot ->
+            for (documentSnapshot in querySnapshot) {
+                val data = documentSnapshot.data
+
+                val latitude = data["latitude"].toString().toDouble()
+                val longitude = data["longitude"].toString().toDouble()
+                val averageDb = data["average"].toString().toDouble()
+
+                val heatMapPoint = WeightedLatLng(LatLng(latitude, longitude), averageDb)
+                heatMapData.add(heatMapPoint)
+
+                val heatmapTileProvider = HeatmapTileProvider.Builder()
+                    .weightedData(heatMapData)
+                    .build()
+
+                mGoogleMap?.addTileOverlay(TileOverlayOptions().tileProvider(heatmapTileProvider))
+            }
+        }.addOnFailureListener { exception ->
+            Log.e("batata", "Error getting documents: $exception")
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
-
-    fun onMapReady(googleMap: GoogleMap) {
-        var mMap = googleMap
-
-        // Crie uma lista de LatLng com os pontos do mapa de calor
-        val heatmapData = mutableListOf<LatLng>()
-        heatmapData.add(LatLng(-33.87365, 151.20689)) // Exemplo de coordenada
-        // Adicione mais pontos conforme necessário
-
-        // Crie o provedor de mapa de calor
-        val heatmapTileProvider = HeatmapTileProvider.Builder()
-            .data(heatmapData)
-            .build()
-
-        // Adicione a camada do mapa de calor ao mapa
-        val heatmapOverlay = mMap.addTileOverlay(TileOverlayOptions().tileProvider(heatmapTileProvider))
     }
 
 }
